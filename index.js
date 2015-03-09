a5a304f6f4f299c279a24c1b7c195efa70348a9c
alfred sang <i5ting@126.com>
2015-03-09 14:56:50 +0800

    增加依赖adb命令

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+require('shelljs/global');
+
+if (!which('adb')) {
+  echo('Sorry, this script requires adb, please put it in PATH');
+  exit(1);
+}
+
 var child_process = require('child_process');
 
 var argv = process.argv;
